34e91cdc3f3151b0c2be7149dade0cf0ead14001
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-18 08:23:52 +0300

    add readme on the specification on the app interaction with the users.

--- README.md
+++ README.md
@@ -30,6 +30,13 @@ for clarification or question find me through my social media  accounts;
 * may j Mackenzie (Instagram)
 
 # Specification
+Behavior	Input	Outcome
+Player 1 inputs name and Player 2 inputs name, and clicks start button.
+Player 1 clicks ROLL button	Click ROLL button	Dice tosses and number is generated at random 1 to 6.
+If Player 1 tosses any number other than 1, that roll is added to round total to sum up his/hers scores.
+If Player 1 toss a 1, no score is added and round for Player 1 ends start a chance for  Player 2 some happens when player 2 toss a 1.
+if either of the players wish not to continue tossing the dice he/she press hold button to sum the total and skip to the net player
+the first player to reach hundred of more than a 100 she/he declared a winner.
 
 # License
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
-$(document).readyfunction(event) {
-    function resetFields() {
-      var playersDetail = [player - 1, player - 2]
-      $("input#new-player-1-name").val("");
-      $("input#new-player-2-name").val("");
-      $(button# newGame).click(function() {
-          $("#newGame").append("players")
-        }
-        var dice = [1, 2, 3, 4, 5, 6]
-        $(button# tose).click()
-      }
+var scores, roundScore, activePlayer, gamePlaying;
+var btnRoll = document.querySelector('.roll');
+var btnHold = document.querySelector('.hold');;
+init();
+
+(document).ready fuction('.roll').addEventListener('click', function() {
+  if (gamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var = .dice
+    document.('#initial-' + activePlayer).innerHTML = ' + dice + ';
+    if (dice !== 1) {
+      ();
+      roundScore += dice;
+      document.querySelector('#initial-' + activePlayer).textContent = roundScore;
+    } else {
+      disableBtn(btnRoll, );
+      hideRolledMsg();
+      nextPlayer();
+    }
+  });
+});
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-rolled-1').style.visibility = 'hidden';
+  document.querySelector('.player-1-rolled-1').style.visibility = 'hidden';
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner-0');
+  document.querySelector('.player-1-panel').classList.remove('winner-1');
+});
