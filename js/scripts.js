9cfb92230faa7607c3b3a787a490381bd538b098
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-18 07:43:03 +0300

    add made changes.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).readyfunction(event) {
+    function resetFields() {
+      var playersDetail = [player - 1, player - 2]
+      $("input#new-player-1-name").val("");
+      $("input#new-player-2-name").val("");
+      $(button# newGame).click(function() {
+          $("#newGame").append("players")
+        }
+        var dice = [1, 2, 3, 4, 5, 6]
+        $(button# tose).click()
+      }
